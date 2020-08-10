35c981334af91510f428273ee96893038641d3eb
Mayur <62636740+mayurborkar@users.noreply.github.com>
2020-08-10 20:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,24 @@ import sklearn
 import pickle
 import pandas as pd
 
-app=Flask(__name__)
-model=pickle.load(open('flight_rf.pkl','rb'))
+app = Flask(__name__)
+model = pickle.load(open("flight_rf.pkl", "rb"))
+
+
 
 @app.route("/")
 @cross_origin()
 def home():
-    return render_template(index.html)
+    return render_template("home.html")
 
-@app.route("/predict",methods=["GET","POST"])
+
+
+
+@app.route("/predict", methods = ["GET", "POST"])
 @cross_origin()
 def predict():
     if request.method == "POST":
-        
+
         # Date_of_Journey
         date_dep = request.form["Dep_Time"]
         Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
@@ -351,10 +356,10 @@ def predict():
 
         output=round(prediction[0],2)
 
-        return render_template('index.html',prediction_text="Your Flight price is Rs. {}".format(output))
+        return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
 
 
-    return render_template("index.html")
+    return render_template("home.html")
 
 
 
